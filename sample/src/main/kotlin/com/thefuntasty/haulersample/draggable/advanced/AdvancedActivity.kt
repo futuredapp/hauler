bc67d2fa827e7484bfe3775f9c82a9eda80ee7f3
Ondrej Kalman <ondrej.kalman@thefuntasty.com>
2019-05-13 10:33:06 +0200

    Feat: animate activity dismiss up and down

--- sample/src/main/kotlin/com/thefuntasty/haulersample/draggable/advanced/AdvancedActivity.kt
+++ sample/src/main/kotlin/com/thefuntasty/haulersample/draggable/advanced/AdvancedActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.thefuntasty.hauler.DragDirection
 import com.thefuntasty.haulersample.R
 import kotlinx.android.synthetic.main.activity_advanced.*
 
@@ -20,9 +21,17 @@ class AdvancedActivity : AppCompatActivity() {
 
         advancedHaulerView.setOnDragDismissedListener { dragDirection ->
             Toast.makeText(this, "Dismissed in direction: $dragDirection", Toast.LENGTH_SHORT).show()
-            finish()
+            when (dragDirection) {
+                DragDirection.DOWN -> {
+                    finish()
+                    overridePendingTransition(0, R.anim.anim_slide_down)
+                }
+                DragDirection.UP -> {
+                    finish()
+                    overridePendingTransition(0, R.anim.anim_slide_up)
+                }
+            }
         }
-
         ignoredAreaView.setScrollViewParent(scrollViewParent)
     }
 }
